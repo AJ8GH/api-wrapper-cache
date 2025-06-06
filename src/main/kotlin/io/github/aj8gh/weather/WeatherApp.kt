eca277e0eedd2099f07566bf06de71c90cb22946
Adam Jonas <adamjonas25@gmail.com>
2025-06-06 23:39:49 +0100

    Catch and log

--- src/main/kotlin/io/github/aj8gh/weather/WeatherApp.kt
+++ src/main/kotlin/io/github/aj8gh/weather/WeatherApp.kt
@@ -1,13 +1,23 @@
 package io.github.aj8gh.weather
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.util.PlaceholderResolutionException
+
+private val logger = KotlinLogging.logger { }
 
 @EnableCaching
 @SpringBootApplication
 class WeatherApp
 
 fun main(args: Array<String>) {
-  runApplication<WeatherApp>(*args)
+  try {
+    runApplication<WeatherApp>(*args)
+  } catch (e: PlaceholderResolutionException) {
+    logger.error {
+      "\n\n\n\n\n===!!!=== placeholder: ${e.placeholder} values: ${e.values} message: ${e.message} stacktrace: ${e.stackTrace} ===!!!===\n\n\n\n\n"
+    }
+  }
 }
