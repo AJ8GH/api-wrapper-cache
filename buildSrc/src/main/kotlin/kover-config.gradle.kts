8cbba7532e87969bd0b75bcdfc9aa98ea6bd1a62
Adam Jonas <adamjonas25@gmail.com>
2025-06-08 21:52:18 +0100

    Refactor kover config script

--- buildSrc/src/main/kotlin/kover-config.gradle.kts
+++ buildSrc/src/main/kotlin/kover-config.gradle.kts
@@ -1,28 +1,45 @@
+import org.w3c.dom.Node
 import javax.xml.parsers.DocumentBuilderFactory
+
+val formatString = "%.1f"
+val defaultPercentage = 0.0
 
 tasks.register("printLineCoverage") {
   group = "verification"
   description = "Generates the coverage badge"
   dependsOn("koverXmlReport")
   doLast {
-    val report = file("$projectDir/build/reports/kover/report.xml")
-    val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(report)
-    var childNode = doc.firstChild.firstChild
-    var coveragePercent = 0.0
-    while (childNode != null) {
-      if (childNode.nodeName == "counter") {
-        val typeAttr = childNode.attributes.getNamedItem("type")
-        if (typeAttr.textContent == "LINE") {
-          val missedAttr = childNode.attributes.getNamedItem("missed")
-          val coveredAttr = childNode.attributes.getNamedItem("covered")
-          val missed = missedAttr.textContent.toLong()
-          val covered = coveredAttr.textContent.toLong()
-          coveragePercent = (covered * 100.0) / (missed + covered)
-          break
-        }
-      }
-      childNode = childNode.nextSibling
-    }
-    println("%.1f".format(coveragePercent))
+    println(formatString.format(getCoverage(getFirstDocumentNode())))
   }
 }
+
+fun getFirstDocumentNode(): Node? = DocumentBuilderFactory
+  .newInstance()
+  .newDocumentBuilder()
+  .parse(file("$projectDir/build/reports/kover/report.xml"))
+  .firstChild
+  .firstChild
+
+fun getCoverage(node: Node?): Double {
+  var currentNode = node
+  while (currentNode != null) {
+    if (currentNode.nodeName == "counter" && isLine(currentNode)) {
+      return calculateCoverage(currentNode)
+    }
+    currentNode = currentNode.nextSibling
+  }
+  return defaultPercentage
+}
+
+fun calculateCoverage(node: Node): Double {
+  val missedAttr = node.attributes.getNamedItem("missed")
+  val coveredAttr = node.attributes.getNamedItem("covered")
+  val missed = missedAttr.textContent.toLong()
+  val covered = coveredAttr.textContent.toLong()
+  return (covered * 100.0) / (missed + covered)
+}
+
+fun isLine(node: Node) = node
+  .attributes
+  .getNamedItem("type")
+  .textContent == "LINE"
