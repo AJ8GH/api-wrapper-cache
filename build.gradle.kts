1cc68adae24c45b22d1d4c3633b02cfdf2302052
Adam Jonas <adamjonas25@gmail.com>
2025-06-08 21:04:11 +0100

    Create buildSrc module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
-import javax.xml.parsers.DocumentBuilderFactory
-
 plugins {
   alias(libs.plugins.java.library)
   alias(libs.plugins.kotlin.jvm)
   alias(libs.plugins.kotlin.spring)
   alias(libs.plugins.kover)
+  alias(libs.plugins.kover.config)
   alias(libs.plugins.spring.boot)
   alias(libs.plugins.spring.dependency.management)
 }
@@ -12,6 +11,7 @@ plugins {
 repositories {
   mavenLocal()
   mavenCentral()
+  gradlePluginPortal()
 }
 
 group = properties["project.group.id"]!!
@@ -39,35 +39,11 @@ kover {
   reports {
     filters {
       excludes {
-        classes("$group.weather.WeatherApp", "$group.weather.WeatherAppKt")
+        classes(
+          "$group.weather.WeatherApp",
+          "$group.weather.WeatherAppKt"
+        )
       }
     }
-  }
-}
-
-tasks.register("printLineCoverage") {
-  group = "verification" // Put into the same group as the `kover` tasks
-  description = "Generates the coverage badge"
-  dependsOn("koverXmlReport")
-  doLast {
-    val report = file("$projectDir/build/reports/kover/report.xml")
-    val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(report)
-    var childNode = doc.firstChild.firstChild
-    var coveragePercent = 0.0
-    while (childNode != null) {
-      if (childNode.nodeName == "counter") {
-        val typeAttr = childNode.attributes.getNamedItem("type")
-        if (typeAttr.textContent == "LINE") {
-          val missedAttr = childNode.attributes.getNamedItem("missed")
-          val coveredAttr = childNode.attributes.getNamedItem("covered")
-          val missed = missedAttr.textContent.toLong()
-          val covered = coveredAttr.textContent.toLong()
-          coveragePercent = (covered * 100.0) / (missed + covered)
-          break
-        }
-      }
-      childNode = childNode.nextSibling
-    }
-    println("%.1f".format(coveragePercent))
   }
 }
--- buildSrc/src/main/kotlin/kover-config.gradle.kts
+++ buildSrc/src/main/kotlin/kover-config.gradle.kts
@@ -1,38 +1,28 @@
 import javax.xml.parsers.DocumentBuilderFactory
 
-// IMPORTANT! Must be defined in the plugins block:
-// plugins { id("org.jetbrains.kotlinx.kover") version ... }
-
 tasks.register("printLineCoverage") {
-  group = "verification" // Put into the same group as the `kover` tasks
+  group = "verification"
+  description = "Generates the coverage badge"
   dependsOn("koverXmlReport")
   doLast {
-    val report = file("$buildDir/reports/kover/xml/report.xml")
-
+    val report = file("$projectDir/build/reports/kover/report.xml")
     val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(report)
-    val rootNode = doc.firstChild
-    var childNode = rootNode.firstChild
-
+    var childNode = doc.firstChild.firstChild
     var coveragePercent = 0.0
-
     while (childNode != null) {
       if (childNode.nodeName == "counter") {
         val typeAttr = childNode.attributes.getNamedItem("type")
         if (typeAttr.textContent == "LINE") {
           val missedAttr = childNode.attributes.getNamedItem("missed")
           val coveredAttr = childNode.attributes.getNamedItem("covered")
-
           val missed = missedAttr.textContent.toLong()
           val covered = coveredAttr.textContent.toLong()
-
           coveragePercent = (covered * 100.0) / (missed + covered)
-
           break
         }
       }
       childNode = childNode.nextSibling
     }
-
     println("%.1f".format(coveragePercent))
   }
 }
